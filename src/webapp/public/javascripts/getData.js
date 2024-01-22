de453695a8e7f33f483d5bdd241672b32f736d4d
fgessner <finn.gessner@uni-muenster.degit config --list>
2024-01-22 12:31:59 +0100

    weekly page + func to get dates data

--- src/webapp/public/javascripts/getData.js
+++ src/webapp/public/javascripts/getData.js
@@ -2,13 +2,17 @@
 
 async function readTextareas() 
 {
+    let nowTimestamp = Date.now();
+    console.log(nowTimestamp);
+    let nowDate = new Date(nowTimestamp);
+    console.log(nowDate);
+
     const sensebox = document.getElementById(`sensebox`).value;
     const leftSensor = document.getElementById(`leftSensor`).value;
     const midSensor = document.getElementById(`midSensor`).value;
     const rightSensor = document.getElementById(`rightSensor`).value;
 
-    let nowTimestamp = Date.now();
-    let nowDate = new Date(nowTimestamp);
+    
 
     let fortyFiveMinutesAgoTimestamp = nowTimestamp - (45 * 60 * 1000);
     let fortyFiveMinutesAgoDate = new Date(fortyFiveMinutesAgoTimestamp);
@@ -52,12 +56,78 @@ async function readTextareas()
         localStorage.setItem("leftData", JSON.stringify(leftData));
         localStorage.setItem("midData", JSON.stringify(midData));
         localStorage.setItem("rightData", JSON.stringify(rightData));
-        window.location.href = '/short_vis'
     }
       
+}
+
+
+
+async function readTextareasWeekly(){
+
+    const sensebox = document.getElementById(`sensebox`).value;
+    const leftSensor = document.getElementById(`leftSensor`).value;
+    const midSensor = document.getElementById(`midSensor`).value;
+    const rightSensor = document.getElementById(`rightSensor`).value;
+
+
+    const date = document.getElementById("dateSelector").value;
+    const dateBegin = date + "16:00:00 GMT+0100";
+    const earlyDate =  new Date(dateBegin);
+    
+    const nextDate = new Date (date.getTime() + 86400000);
+    const dateEnd = nextDate + "02:00:00 GMT+0100";
+    const lateDate = new Date (dateEnd); 
+
+
+    const boxURL = "https://api.opensensemap.org/boxes/" + sensebox + "/sensors";
+    const leftURL = "https://api.opensensemap.org/boxes/" + sensebox + "/data/" + leftSensor + "?from-date=" + earlyDate.toISOString() + "&to-date=" + lateDate.toISOString() + "&download=false&format=json";
+    const midURL = "https://api.opensensemap.org/boxes/" + sensebox + "/data/" + midSensor + "?from-date=" + earlyDate.toISOString() + "&to-date=" + lateDate.toISOString() + "&download=false&format=json";
+    const rightURL = "https://api.opensensemap.org/boxes/" + sensebox + "/data/" + rightSensor + "?from-date=" + earlyDate.toISOString() + "&to-date=" + lateDate.toISOString() + "&download=false&format=json";
+
+    const boxResponse = await fetch (boxURL);
+    const leftResponse = await fetch (leftURL);
+    const midResponse = await fetch (midURL);
+    const rightResponse = await fetch (rightURL);
+
+    if (boxResponse.status != 200)
+    {
+        console.log("sensebox-ID is invalid");
+    }
+    else if (leftResponse.status != 200)
+    {
+        console.log("left Sensor-ID is invalid");
+    }
+    else if (midResponse.status != 200)
+    {
+        console.log("mid Sensor-ID is invalid");
+    }
+    else if (rightResponse.status != 200)
+    {
+        console.log("right Sensor-ID is invalid");
+    }
+    else
+    {
+        let leftData = await leftResponse.json();
+        let midData = await midResponse.json();
+        let rightData = await rightResponse.json();
+
+        //console.log(leftData)
+
+        localStorage.setItem("leftDataWeeklyy", JSON.stringify(leftData));
+        localStorage.setItem("midDataWeekly", JSON.stringify(midData));
+        localStorage.setItem("rightDataWeekly", JSON.stringify(rightData));
+    }
 }
   
 document.addEventListener('DOMContentLoaded', function () 
 {
-    document.getElementById('confirm').addEventListener('click', readTextareas);
+    document.getElementById('live').addEventListener('click', function() {
+        readTextareas();
+        window.location.href = '/short_vis'
+    });
+
+    document.getElementById('weekly').addEventListener('click', function(){
+        readTextareasWeekly();
+        window.location.href = '/weekly_vis'
+    })
 });
