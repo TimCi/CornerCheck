200e4b83a27c8ad4a1cd1779863aaf5a2d350225
fgessner <finn.gessner@uni-muenster.degit config --list>
2024-01-22 12:32:33 +0100

    rest

--- src/webapp/app.js
+++ src/webapp/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var shortVisRouter = require('./routes/visualize45');
+var weeklyVisRouter= require('./routes/visualizeWeekly')
 
 var app = express();
 
@@ -22,7 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/short_vis', shortVisRouter)
+app.use('/short_vis', shortVisRouter);
+app.use('/weekly_vis', weeklyVisRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
